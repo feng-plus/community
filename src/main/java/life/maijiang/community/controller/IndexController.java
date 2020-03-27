dc65466a18df9a8bddb53e3c5dd4230cf5c94c58
feng-plus <1725909232@qq.com>
2020-03-27 23:28:11 +0800

    add index

--- src/main/java/life/maijiang/community/controller/IndexController.java
+++ src/main/java/life/maijiang/community/controller/IndexController.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("hello")
-    public String hello(@RequestParam(name="name",defaultValue = "hello World")String name, Model model){
-        System.out.println(name);
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
